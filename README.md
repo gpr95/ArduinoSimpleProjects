ab49757d1b1a4811cd80078da02ed849417d91e8
GPR-LENOVO-\gpr <gprzytula95@gmail.com>
2017-05-04 18:19:13 +0200

    Pir skatch + png

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # ArduinoSimpleProjects
 Smark Home
+## PIR With Arduino Mini Pro and RF24
+### Functionality
+0. Begin communication at 9600 bps
+1. Every loop read pir output value
+2. Send 0(nothing)/1(motion) **ONCE** on 90 channel to other RF24 receiver in payload
+```
+struct payloadRF24Msg 
+{
+  unsigned int sensorValue;
+};
+```
+3. Print sending value on serial bus
+### Connections
+![Alt text](pir_arduinoMiniPro_rf24/connections.png?raw=true "Connections")
---
+++ pir_arduinoMiniPro_rf24/pir_arduinoMiniPro_rf24/pir_arduinoMiniPro_rf24.ino
@@ -0,0 +1,75 @@
+#include <RF24.h>
+#include <RF24Network.h>
+#include <SPI.h>
+
+RF24 radio24(7,8);
+RF24Network network(radio24);
+
+/** RF24 IDs */
+const uint16_t pirNodeId = 01;
+const uint16_t serverNodeId = 00;
+
+/** Analog input pin that the PIR is attached too */
+const int pirDigitalPIN = 4; 
+ 
+/** Value read from the pir values = HIRH/LOW */
+int pirSensorValue = 0;  
+
+/** Bool for discriminate whether high value was sended */ 
+bool highValueWasSended = false;
+
+/** RF24 send scruct */
+struct payloadRF24Msg 
+{
+  unsigned int sensorValue;
+};
+
+void setup() 
+{
+  /** Initialize serial communications at 9600 bps */
+  Serial.begin(9600);
+  /** Initialize SPI pins for RF24 module */
+  SPI.begin();
+
+  radio24.begin();
+  /** Channel 90 communication from this node */
+  network.begin(90, pirNodeId);
+}
+
+void loop() 
+{
+  /** Update network state every loop */
+  network.update();
+  /** Read PIR value every loop */
+  pirSensorValue = digitalRead(pirDigitalPIN);
+  
+  if(pirSensorValue == HIGH) 
+      sendValueThroughRF24(1);
+  else
+      sendValueThroughRF24(0);
+}
+
+void sendValueThroughRF24(unsigned int val) 
+{
+  if(val == 1 && highValueWasSended)
+    return;
+  if(val == 0 && !highValueWasSended)
+    return;
+  Serial.println("SENDING : %d", val);
+  RF24NetworkHeader header(serverNodeId);
+  payloadRF24Msg payload = {val};
+  bool ok = network.write(header, &payload, sizeof(payload));
+  if(ok) {
+    Serial.println(" ok. Sended");
+    if(val == 1) {
+      highValueWasSended = true;
+    }
+    else {
+      highValueWasSended = false;
+    }
+  }
+  else {
+    Serial.println(" FAILED.");
+  }
+}
+
